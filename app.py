f4a03be6ba50cbf4009d435c4846f5c1689895e5
Alen Krga <alen.krga1@gmail.com>
2020-08-05 18:04:10 +0200

    Created a Flask app

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect, url_for
+from bson.objectid import ObjectId
+from flask_pymongo import PyMongo
+import os
+try:
+	import env
+except:
+	pass
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+	app.debug = os.environ.get('DEBUG') == 'TRUE'
+	app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT'))
+        	)
